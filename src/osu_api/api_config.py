eaeef73ea4ccd419d54fc4373292d25352e08e45
RShields <playnow254@gmail.com>
2021-04-15 10:31:05 -0700

    Fix api_config
    Fix naming conflicts

--- src/osu_api/api_config.py
+++ src/osu_api/api_config.py
@@ -2,13 +2,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client_id = os.environ["CLIENT_ID"]
-client_secret = os.environ["CLIENT_SECRET"]
+_client_id = os.environ["CLIENT_ID"]
+_client_secret = os.environ["CLIENT_SECRET"]
 
 
 def client_id():
-    return client_id
+    return _client_id
 
 
 def client_secret():
-    return client_secret
+    return _client_secret
