25995dfcbd0585fadd1a3a2d711720fad76cdbda
RShields <playnow254@gmail.com>
2021-04-15 18:12:30 -0700

    Add update to player

--- src/osu_api/player.py
+++ src/osu_api/player.py
@@ -1,4 +1,14 @@
+from ..osu_api import api_helper
+
+
 class Player:
-    def __init__(self, json):
-        self.id = json["id"]
-        self.username = json["username"]
+    def __init__(self, player_json):
+        self.player_id = player_json["id"]
+        self.username = player_json["username"]
+        self.rank = player_json["statistics"]["global_rank"]
+
+    def update(self) -> None:
+        player = api_helper.get_player_by_id(self.player_id)
+        self.player_id = player.player_id
+        self.username = player.username
+        self.rank = player.rank
