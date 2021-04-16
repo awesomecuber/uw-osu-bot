cd72b2a56c61fcd359fcfb1a5fb43f41c0c2ec6e
RShields <playnow254@gmail.com>
2021-04-16 06:11:42 -0700

    Fix leaderboards

--- src/discord/leaderboards.py
+++ src/discord/leaderboards.py
@@ -1,59 +1,87 @@
+from typing import Dict, Any
+
+from ..osu_api.score import Score
+from ..tournament.person import Person
 from ..tournament.tournament_state import TournamentState
-from ..utils.calculate_score import calculate_score
+from ..utils.calculate_points import calculate_points
+from ..utils.sanitizer import sanitize
 
 
-def get_total_leaderboards(players):
-    player_scores = get_player_scores(players)
+# Sum players' normalized scores
+def get_total_leaderboards(people: list[Person]):
+    tournamentmaps = TournamentState.instance.tournamentmaps
+    beatmapset_ids = [tournamentmap.beatmapset.beatmapset_id for tournamentmap in tournamentmaps]
 
-    # v[1] is the regularized score, add those up for each map
-    final_scores = {
-        username: sum([v[1] for v in score_data.values()])
-        for (username, score_data) in player_scores.items()
-    } # map from username to total score
+    people_norm_pointss = get_people_normalized_points(people, beatmapset_ids)
+    sorted_people_norm_pointss = sort_dict(people_norm_pointss)
 
     message = ""
-    for i, (username, total_score) in enumerate(sorted(final_scores.items(), key=lambda x: x[1], reverse=True)):
-        message += f"{i+1}. {username} ({total_score:.1f})\n"
+    for i in range(len(people)):
+        person = sorted_people_norm_pointss[i][0]
+        username = sanitize(person.player.username)
+        norm_points = sorted_people_norm_pointss[i][1]
+        message += f"{i+1}. {username} ({norm_points:.1f})\n"
     return message
 
 
-def get_map_leaderboards(players):
+# Display players' raw and normalized scores
+def get_map_leaderboards(people: list[Person]):
     message = ""
-    player_scores = get_player_scores(players)
-    for beatmapset in TournamentState.instance.beatmaps.values():
-        message += f"__{beatmapset.ascii_name}__\n"
+    for tournamentmap in TournamentState.instance.tournamentmaps:
+        beatmapset = tournamentmap.beatmapset
+        beatmapset_id = beatmapset.beatmapset_id
 
-        for i, (username, score_data) in enumerate(
-                    sorted(player_scores.items(), key=lambda x: x[1][mapset_id][0], reverse=True)
-                ):
-            score, normalized = score_data[mapset_id]
-            message += f"{i+1}. {username}: {score:.1f} (normalized: {normalized:.1f})\n"
+        pointss = get_beatmapset_pointss(people, beatmapset_id)
+        norm_pointss = get_beatmapset_normalized_points(people, beatmapset_id)
+        sorted_norm_pointss = sort_dict(norm_pointss)
+
+        message += f"__{sanitize(beatmapset.ascii_name)}__\n"
+
+        for i in range(len(people)):
+            person = people[i]
+            username = sanitize(person.player.username)
+
+            points = pointss.get(person)
+            norm_points = sorted_norm_pointss[i]
+
+            message += f"{i+1}. {username}: {points:.1f} (normalized: {norm_points:.1f})\n"
         message += "\n"
     return message
 
 
-# returns map from username to (map id to (score, normalized))
-def get_player_scores(players):
-    to_return = {}
-    for player_data in players.values():
-        scores = {}
-        for map_id, score_data in player_data["scores"].items():
-            scores[map_id] = calculate_score(score_data["pp"], score_data["sr"])
-        to_return[player_data["username"]] = scores
+def get_beatmapset_scores(people: list[Person], beatmapset_id) -> Dict[Person, Score]:
+    return {person: person.scores.get(beatmapset_id) for person in people}
 
-    # add in normalized
-    best_scores = {}
-    for beatmapset_id in TournamentState.instance.beatmaps.values():
-        if len(to_return) == 0:
-            best_score = 0
-        else:
-            best_score = max(v[beatmapset_id] for (_, v) in to_return.items())
 
-        if best_score == 0:
-            best_score = 1 # to prevent divide by zero
-        best_scores[beatmapset_id] = best_score
+def get_beatmapset_pointss(people: list[Person], beatmapset_id) -> Dict[Person, float]:
+    scores = get_beatmapset_scores(people, beatmapset_id)
+    return {person: calculate_points(scores.get(person)) for person in people}
 
-    for player_name, player_data in to_return.items():
-        for map_id, score in player_data.items():
-            to_return[player_name][map_id] = (score, 250 * (score / best_scores[map_id]))
-    return to_return
+
+def get_beatmapset_normalized_points(people: list[Person], beatmapset_id) -> Dict[Person, float]:
+    pointss = get_beatmapset_pointss(people, beatmapset_id)
+
+    max_points = max(pointss.values())
+    # prevent divide by 0 errors
+    if max_points == 0:
+        max_points = 1
+
+    # most possible points for this beatmapset
+    normalization_constant = 1000.0 / len(TournamentState.instance.tournamentmaps)
+
+    return {person: normalization_constant * pointss.get(person) / max_points for person in people}
+
+
+def get_people_normalized_points(people: list[Person], beatmapset_ids: list) -> Dict[Person, float]:
+    output = {person: 0 for person in people}
+    for beatmapset_id in beatmapset_ids:
+        beatmapset_norm_pointss = get_beatmapset_normalized_points(people, beatmapset_id)
+        for person in people:
+            output[person] += beatmapset_norm_pointss[person]
+    return output
+
+
+# Sorts a dict from highest float value to lowest
+def sort_dict(in_dict: Dict[Any, float]) -> list[Any, float]:
+    in_list = list(in_dict)
+    return sorted(in_list, key=lambda entry: entry[1], reverse=True)
