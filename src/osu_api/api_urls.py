8f3456f95eea26a9ba256ffa12bea5b13a0029ef
RShields <playnow254@gmail.com>
2021-04-15 21:19:30 -0700

    Add more descriptive names to api_urls

--- src/osu_api/api_urls.py
+++ src/osu_api/api_urls.py
@@ -5,17 +5,17 @@ def token() -> str:
     return "https://osu.ppy.sh/oauth/token"
 
 
-def user_by_username(username: str) -> str:
-    return base_url + f"users/{username}/osu"
+def user_by_username(player_name: str) -> str:
+    return base_url + f"users/{player_name}/osu"
 
 
-def user_by_id(id) -> str:
-    return base_url + f"users/{id}"
+def user_by_id(player_id) -> str:
+    return base_url + f"users/{player_id}"
 
 
-def recent_scores_by_id(id) -> str:
-    return base_url + f"users/{id}/scores/recent"
+def recent_scores_by_id(player_id) -> str:
+    return base_url + f"users/{player_id}/scores/recent"
 
 
-def beatmapset(id) -> str:
-    return base_url + f"beatmapsets/{id}"
+def beatmapset(beatmapset_id) -> str:
+    return base_url + f"beatmapsets/{beatmapset_id}"
