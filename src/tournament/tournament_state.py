8e3978340a3659b9e563c012cfebc4be062aaa4d
RShields <playnow254@gmail.com>
2021-04-15 21:14:27 -0700

    Add var types to tournament_state

--- src/tournament/tournament_state.py
+++ src/tournament/tournament_state.py
@@ -1,15 +1,18 @@
+from typing import Dict, Optional
+
 from person import Person
+from tournament_map import TournamentMap
 
 
 class TournamentState:
-    instance = None
+    instance = None  # type: TournamentState
 
     def __init__(self):
         # player_id : Person
-        self.pros = {}
-        self.amateurs = {}
+        self.pros = {}  # type: Dict[str: Person]
+        self.amateurs = {}  # type: Dict[str: Person]
         # beatmapset_id : Beatmapset
-        self.beatmaps = {}
+        self.beatmaps = {}  # type: Dict[str: TournamentMap]
         TournamentState.instance = self
 
     def register(self, person: Person) -> None:
@@ -17,6 +20,19 @@ class TournamentState:
             self.pros[person.player.player_id] = person
         else:
             self.amateurs[person.player.player_id] = person
+
+    def unregister(self, player_id) -> None:
+        if player_id in self.pros:
+            self.pros.pop(player_id)
+        elif player_id in self.amateurs:
+            self.amateurs.pop(player_id)
+
+    def get_person_from_player_id(self, player_id) -> Optional[Person]:
+        if player_id in self.pros:
+            return self.pros.get(player_id)
+        elif player_id in self.amateurs:
+            return self.amateurs.get(player_id)
+        return None
 
     def update_ranks(self) -> None:
         people = []
