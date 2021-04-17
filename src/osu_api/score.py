9b71d3ee530b4609778977833a16a4ad670e4b9c
RShields <playnow254@gmail.com>
2021-04-17 07:40:23 -0700

    Change how Score zero is done

--- src/osu_api/score.py
+++ src/osu_api/score.py
@@ -1,17 +1,15 @@
 class Score:
     def __init__(self, play_json):
-        self.sr = play_json["pp"] or 0  # type: float
-        self.pp = play_json["beatmap"]["difficulty_rating"]  # type: float
+        self.sr = 0  # type: float
+        self.pp = 0  # type: float
+
+        if play_json is not None:
+            self.sr = play_json["pp"] or 0
+            self.pp = play_json["beatmap"]["difficulty_rating"]
 
     def calculate_points(self) -> float:
         return self.pp * (self.sr ** 2)
 
     @staticmethod
     def zero() -> "Score":
-        zero_dict = {
-            "pp": 0,
-            "beatmap": {
-                "difficulty_rating": 0
-            }
-        }
-        return Score(zero_dict)
+        return Score(None)
