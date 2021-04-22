28702d19662056bef22593e06d7e5e6994f193f9
nbrass <nbrass@uw.edu>
2021-04-21 17:46:56 -0700

    use modern typing features

--- src/osu_api/beatmapset.py
+++ src/osu_api/beatmapset.py
@@ -1,8 +1,8 @@
 class Beatmapset:
     def __init__(self, beatmapset_json):
-        self.beatmapset_id = beatmapset_json["id"]  # type: int
-        self.ascii_name = beatmapset_json["title"]  # type: str
-        self.unicode_name = beatmapset_json["title_unicode"]  # type: str
-        self.difficulty_count = len(beatmapset_json["beatmaps"])  # type: int
+        self.beatmapset_id: int = beatmapset_json["id"]
+        self.ascii_name: str = beatmapset_json["title"]
+        self.unicode_name: str = beatmapset_json["title_unicode"]
+        self.difficulty_count: int = len(beatmapset_json["beatmaps"])
         beatmap_jsons = beatmapset_json["beatmaps"]
-        self.max_sr = max(beatmap_json["difficulty_rating"] for beatmap_json in beatmap_jsons)  # type: float
+        self.max_sr: float = max(beatmap_json["difficulty_rating"] for beatmap_json in beatmap_jsons)
