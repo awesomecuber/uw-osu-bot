62ad8a0bd0f8e36d3efbaef52429c6b0953bc128
nbrass <nbrass@uw.edu>
2021-04-28 02:38:19 -0700

    temp fix sanitize

--- uwosubot/utils/sanitizer.py
+++ uwosubot/utils/sanitizer.py
@@ -2,4 +2,9 @@ import re
 
 
 def sanitize(s: str) -> str:
-    return re.sub(r"([*_~`>])", r"\1", s)
+    underscore_index = s.find("_")
+    if underscore_index != -1:
+        return s[:underscore_index] + "\\" + s[underscore_index:]
+    return s
+    # NOT WORKING
+    # return re.sub(r"([*_~`>])", r"\1", s)
